692536d4c7b1f4d62806b9f7d8afb6af83a61139
mauroluciano <mauroluciano911@gmail.com>
2023-10-27 12:18:07 +0200

    push

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,9 +1,23 @@
-@extends('layout.app')
+@extends('layouts.app')
 @section('content')
-<div>
+<div class="container">
     @foreach ($projects as $project)
-          @dump($project)
-  
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+            </tr>
+        </thead>
+        <tbody>
+        <td>{{$project->title}}</td>
+        <td>{{$project->content}}</td> 
+    </tr>
+
+
+
+</tbody>
+</table> 
     @endforeach
 </div>
 @endsection
